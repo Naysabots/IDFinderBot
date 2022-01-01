ce7747af0b7296760e9342652dfa213da4cb8233
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-01 16:18:15 +0530

    Update config.py

--- NaysaIDBot/config.py
+++ NaysaIDBot/config.py
@@ -3,16 +3,7 @@ import os
 class Config(object):
      
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-
     API_ID = int(os.environ.get("API_ID", 12345))
-
-
     API_HASH = os.environ.get("API_HASH", "")
-
-
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "NaysaBots")
-
-
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "NaysaIDFinderBot")
 
